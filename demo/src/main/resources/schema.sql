cfc33254d9fae512b6c011f9281b4244c3c8c464
IsraelGarciaBetancourt <60408504@continental.edu.pe>
2025-11-04 15:54:59 -0500

    Versión 2 de la BD
    Precio Unitario en Producto_Catalogo

--- demo/src/main/resources/schema.sql
+++ demo/src/main/resources/schema.sql
@@ -1,27 +1,3 @@
-CREATE TABLE IF NOT EXISTS estudiante (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(200) NOT NULL,
-    apellido VARCHAR(200) NOT NULL,
-    codigo VARCHAR(10) UNIQUE,
-    fecha_nacimiento DATE NOT NULL
-);
-
-create table if not exists tipo_producto(
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    nombre varchar(200) not null,
-    fechaCreacion date not null
-);
-
-CREATE TABLE IF NOT EXISTS producto (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    nombre VARCHAR(200) NOT NULL,
-    fecha_creacion DATE NOT NULL,
-    id_tipo_producto INT NOT NULL,
-    CONSTRAINT fk_tipo_producto FOREIGN KEY (id_tipo_producto) 
-    REFERENCES tipo_producto(id) ON DELETE SET NULL
-);
-
-
 -- ================================================
 -- 🧩 TABLA: USUARIOS
 -- ================================================
@@ -55,6 +31,7 @@ CREATE TABLE IF NOT EXISTS producto_catalogo (
     codigo VARCHAR(50) NOT NULL UNIQUE,
     nombre VARCHAR(200) NOT NULL,
     id_categoria INT NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL,
     activo BOOLEAN DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -101,7 +78,6 @@ CREATE TABLE IF NOT EXISTS documento_detalles (
     id_documento INT NOT NULL,
     id_producto_catalogo INT NOT NULL,
     cantidad INT NOT NULL,
-    precio_unitario DECIMAL(10,2) NOT NULL,
     subtotal DECIMAL(10,2) NOT NULL,
     activo BOOLEAN DEFAULT TRUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
